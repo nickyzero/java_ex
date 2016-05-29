8a2a4fe33feee513aee85e4e32447536f04009af
Nicolas Jaeduck Oh <nicolas@Nicolasui-MacBook-Pro.local>
2016-05-29 21:59:36 +0900

    0529 Study

---
+++ ch2/CharToCode.java
@@ -0,0 +1,11 @@
+class CharToCode {
+    public static void main(String[] args){
+        char ch = 'A';
+        int code = (int)ch;
+        
+        System.out.printf("%c=%d(%#x)%n", ch, code, code);
+        
+        char hch = '가';
+        System.out.printf("%c=%d(%#x)%n", hch, (int)hch, (int)hch);
+    }
+}
---
+++ ch2/SpecialCharEx.java
@@ -0,0 +1,9 @@
+class SpecialCharEx {
+    public static void main(String[] args){
+        System.out.println('\'');
+        System.out.println("abc\t123\t456");
+        System.out.println('\n');
+        System.out.println("\"Hello\"");
+        System.out.println("c:\\");
+    }
+}
